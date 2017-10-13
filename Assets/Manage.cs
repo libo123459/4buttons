0c366679c44d080230511022a797f91d700c0148
LiBo <libo123459@126.com>
2017-10-13 09:24:15 +0800

    Initial commit

---
+++ Assets/Manage.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manage : MonoBehaviour {
+    public static bool isStarted = false;
+    public static bool isFinished = false;
+    public static int pressNum = 0;
+    public static int xPre = 0;
+    public static int yPre = 0;
+    public List<TheBTN> BTNs = new List<TheBTN>();
+    public static List<Text> Nums = new List<Text>();
+
+    public void PressUpBtn()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            BTNs[i].GetComponent<Image>().color = new Color(1,1,1,1);
+            BTNs[i].transform.localScale = new Vector3(1, 1, 1);
+            BTNs[i].isSelected = false;
+        }
+        CheckIfPrint();
+        isStarted = false;
+        pressNum = 0;
+        Nums.Clear();
+    }
+
+    void CheckIfPrint()
+    {
+        if (pressNum == 3)
+        {
+            printNums();
+        }
+    }
+
+    void printNums()
+    {
+        for (int i = 0; i < Nums.Count; i++)
+        {
+            print("The" + i.ToString() + "Num is" + Nums[i].text);
+        }
+    }
+
+    public static void updateTheXandY(int xPos,int yPos)
+    {
+        xPre = xPos;
+        yPre = yPos;
+    }
+    private void OnMouseDown()
+    {
+        
+    }
+    private void OnMouseUp()
+    {
+        
+    }
+    // Use this for initialization
+    void Start () {
+        BTNs[0].xPos = 0;
+        BTNs[0].yPos = 0;
+        BTNs[1].xPos = 1;
+        BTNs[1].yPos = 0;
+        BTNs[2].xPos = 0;
+        BTNs[2].yPos = 1;
+        BTNs[3].xPos = 1;
+        BTNs[3].yPos = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TheBTN.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TheBTN : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerDownHandler,IPointerUpHandler{
+    public int xPos = 0;
+    public int yPos = 0;
+    public bool isSelected = false;
+    public Text _text;
+    Manage _manage;
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _manage.PressUpBtn();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {        
+        isSelected = true;
+
+        Manage.isStarted = true;
+        Manage.updateTheXandY(xPos,yPos);
+        Manage.Nums.Add(_text);
+
+        this.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+        this.transform.localScale = new Vector3(0.9f,0.9f,0.9f);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)//悬停选中
+    {
+
+        if (Manage.pressNum < 3)
+        {
+            if (Manage.isStarted == true && isSelected == false)
+            {
+                if (this.xPos == Manage.xPre || this.yPos == Manage.yPre)
+                {
+                    isSelected = true;
+
+                    this.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+                    this.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+
+                    Manage.updateTheXandY(xPos, yPos);
+                    Manage.Nums.Add(_text);
+                    Manage.pressNum++;
+                }
+            }
+        }        
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)//悬停离开
+    {
+        isSelected = false;
+        this.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Use this for initialization
+    void Start () {
+        _manage = GameObject.Find("Main Camera").GetComponent<Manage>();
+        _text = this.transform.GetChild(0).GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
